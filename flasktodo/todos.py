8b96189d189e4f07ec3439917d2b1eb60da44132
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-03-30 14:06:44 -0400

    heroku link now working

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -5,7 +5,8 @@ from . import db
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+
+@bp.route("/", methods=['GET', 'POST'])
 def index():
     """View for home page which shows list of to-do items."""
 
@@ -16,3 +17,14 @@ def index():
 
     return render_template("index.html", todos=todos)
 
+    # if method=='POST':
+    # def new_todo():
+    #"""adds a new item to the todo list"""
+    # Take the input from the form
+    #new_item = request.form['action']
+    # Insert new description into the table as a new row
+    #cur = db.get_db().cursor()
+    # cur.execute("INSERT INTO todos (description) VALUE (%)",
+    # (new_item))
+
+    # return updated table with submit button
