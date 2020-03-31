c93158941ea8ee07c2689fec1ab08e79b68b020d
Joey Zimmerman <joeyzimmerman17@gmail.com>
2020-03-31 12:42:06 -0400

    Add functionality to add tasks

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, g
 
 from . import db
 
@@ -13,23 +13,23 @@ def index():
     """View for home page which shows list of to-do items."""
 
     cur = db.get_db().cursor()
+
+    if request.method == 'POST':
+        new_task = request.form['action']
+
+        cur.execute('INSERT INTO todos (description, completed, created_at) VALUES (%s, %s, %s)',
+        (new_task, False, datetime.now()))
+        g.db.commit()
+
+
     cur.execute('SELECT * FROM todos')
     todos = cur.fetchall()
+
+
     cur.close()
 
     return render_template("index.html", todos=todos)
 
-    # if method=='POST':
-    # def new_todo():
-    #"""adds a new item to the todo list"""
-    # Take the input from the form
-    #new_item = request.form['action']
-    # Insert new description into the table as a new row
-    #cur = db.get_db().cursor()
-    # cur.execute("INSERT INTO todos (description) VALUE (%)",
-    # (new_item))
-
-    # return updated table with submit button
 
 @bp.route("/completed", methods=['GET', 'POST'])
 def show_completed():
