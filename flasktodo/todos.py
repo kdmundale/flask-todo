0d70b1fda4e92b60b9fb10d6ec99de5b6aac86d2
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-03-31 12:45:51 -0400

    add new task to list

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,6 +1,8 @@
+import os
+import psycopg2
+import psycopg2.extras
 from datetime import datetime
-
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, g
 
 from . import db
 
@@ -11,34 +13,23 @@ bp = Blueprint("todos", __name__)
 @bp.route("/", methods=['GET', 'POST'])
 def index():
     """View for home page which shows list of to-do items."""
-
     cur = db.get_db().cursor()
+    if request.method == 'POST':
+        new_task = request.form['action']
+        cur.execute('INSERT INTO todos (description, completed, created_at) VALUES (%s, %s, %s)',
+                    (new_task, False, datetime.now()))
+        g.db.commit()
     cur.execute('SELECT * FROM todos')
     todos = cur.fetchall()
     cur.close()
-
     return render_template("index.html", todos=todos)
 
-
-@bp.route("/new", methods=['GET', 'POST'])
-def new_todo():
-    if request.method == 'POST':
-        now = datetime.timetz()
-        # """adds a new item to the todo list"""
-        # Take the input from the form
-        new_item = request.form['action']
-    # Insert new description into the table as a new row
-        cur = db.get_db().cursor()
-        cur.execute("INSERT INTO todos (description, completed, created_at) VALUE (%,%,%)",
-                    (new_item, False, now))
-        cur.execute('SELECT * FROM todos')
-        todos = cur.fetchall()
-        cur.close()
-
+    return render_template("index.html", todos=todos)
     # return updated table with submit button
     return render_template("index.html")
 
 
+@bp.route("/new", methods=['GET', 'POST'])
 @bp.route("/completed", methods=['GET', 'POST'])
 def show_completed():
     """View for specific completed tasks."""
